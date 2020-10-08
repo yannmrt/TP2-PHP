53acb2684aa63ead2721054eef06a8b892128ffd
Yann Martin <ymartin@la-providence.net>
2020-10-08 11:11:07 +0200

    first commit

---
+++ exo1.php
@@ -0,0 +1,28 @@
+<?php
+
+function formulaire($valeur1, $valeur2) {
+    echo '<table border="1"><tr>';
+    echo '<td>' . $valeur1 . '</td>';
+    echo '<td>' . $valeur2 . '</td>';
+    echo '</tr>';
+
+}
+
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+<title>Exo 1 - TP2</title>
+</head>
+<body>
+
+<?php echo formulaire("valeur1", "valeur2"); ?>
+
+</body>
+</html>
+<?php 
+
+// code source de la page 
+highlight_file(__FILE__); 
+
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head> 
+   <title>Exercices PHP TP 2</title>
+</head>
+<body>
+
+<h1>TP 1</h1>
+<a href="exo1.php"><p>Exercice 1</p></a>
+<a href="exo2a.php"><p>Exercice 2a</p></a>
+<a href="exo2b.php"><p>Exercice 2b</p></a>
+<a href="exo2-2.php"><p>Exercice 2.2</p></a>
+<a href="exo3.php"><p>Exercice 3</p></a>
+<a href="exo4.php"><p>Exercice 4</p></a>
+<a href="exo5.php"><p>Exercice 5</p></a>
+<a href="exo6.php"><p>Exercice 6</p></a>
+<a href="exo7.php"><p>Exercice 7</p></a>
+<a href="exofinal.php"><p>Exercice final</p></a>
+
+</body>
+</html>
